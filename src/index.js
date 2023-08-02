c863ca55733b6e558a7dbc9e8886ef104b253dfe
Greg <Jariye05@gmail.com>
2023-08-02 16:47:50 -0700

    moved src up

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+//import, file name and path for other js files
+
+export class planet {
+  constructor (earthage, futurebirth, passbirth) {
+    
+    this.earthage= earthage;
+    this.futurebirth= futurebirth
+    this.passbirth = passbirth
+}
+
+  getAge()  {
+    this.mercuryAge =
+    this.venusAge
+    this.marsAge
+    this.jupiterAge
+  }
+
+} 
