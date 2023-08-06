333b00a064ec5a51bac3bee5ead2aef7f455e82a
Greg <Jariye05@gmail.com>
2023-08-05 22:19:17 -0700

    added passAge

--- src/planet.js
+++ src/planet.js
@@ -9,11 +9,11 @@ export class Planet {
   getAge() {
     this.mercuryAge = Math.floor(this.earthage / .24);
     this.venusAge= Math.floor(this.earthage /  .62);
-    this.marsAge = Math.floor(this.earthage /  1.88)
-    this.jupiterAge = Math.floor(this.earthage /  11.86)
+    this.marsAge = Math.floor(this.earthage /  1.88);
+    this.jupiterAge = Math.floor(this.earthage /  11.86);
   }
 
   passAge()
-  this.mercuryAge= this.passbirth-
+  this.passAge = this.earthage - this.mercuryAge
 
 }
