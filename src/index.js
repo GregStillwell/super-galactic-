9c1222acac02275eda2615819f76c6e5be70f337
Greg <Jariye05@gmail.com>
2023-08-06 01:36:37 -0700

    added parseint to input

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ function handleForm(event) {
   event.preventDefault()
 
   const userAge = parseInt(document.getElementById("userAge"));
-  const futureAge = parseInt(document.getElementById(futureAge))
-  const pastAge 
+  const futureAge = parseInt(document.getElementById("futureAge"))
+  const pastAge = parseInt(document.getElementById("pastAge"))
   
+  let planet = new Planet(userAge, futureAge, pastAge);
 }
