abe73cc044232fa661dd504d5504cddbe59fecf6
Greg <Jariye05@gmail.com>
2023-08-06 03:39:10 -0700

    fix ids and README

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import './css/styles.css';
 import { Planet } from './planet';
 
 function handleForm(event) {
-  event.preventDefault()
+  event.preventDefault();
 
-  const userAge = parseInt(document.getElementById("userAge"));
-  const futureAge = parseInt(document.getElementById("futureAge"))
-  const pastAge = parseInt(document.getElementById("pastAge"))
+  const userAge = parseInt(document.getElementById("userage"));
+  const futureAge = parseInt(document.getElementById("futureage"));
+  const pastAge = parseInt(document.getElementById("pastage"));
   
   let planet = new Planet(userAge, futureAge, pastAge);
+  planet.getAge();
+  planet.passAge();
+  planet.yearsToPass();
+  planet.yearsPassed();
+  planet.yearsToPassCal();
+
+  let result = document.getElementById("results")
+  result.p.append
+
+ // const p = document.createElement("p");
+ // p.append(results);
+  //document.body.append(p)
 }
+window.addEventListener("load", function() {
+  this.document.getElementById("AgeInput").addEventListener("submit", handleForm)
+});
--- src/planet.js
+++ src/planet.js
@@ -2,37 +2,39 @@ export class Planet {
   constructor(earthage, futurebirth, passbirth) {
 
     this.earthage = earthage;
-    this.futurebirth = futurebirth
-    this.passbirth = passbirth
+    this.futurebirth = futurebirth;
+    this.passbirth = passbirth;
+    
   }
 
-  getAge() {
-    this.mercuryAge = Math.floor(this.earthage / .24);
-    this.venusAge= Math.floor(this.earthage /  .62);
-    this.marsAge = Math.floor(this.earthage /  1.88);
-    this.jupiterAge = Math.floor(this.earthage /  11.86);
-  }
+   getAge() {
+     this.mercuryAge = Math.floor(this.earthage / .24);
+     this.venusAge= Math.floor(this.earthage /  .62);
+     this.marsAge = Math.floor(this.earthage /  1.88);
+     this.jupiterAge = Math.floor(this.earthage /  11.86);
+     
+   }
 
-  passAge() {
-    this.passAgeCal = this.earthage - this.passbirth;
-    console.log(this.passAgeCal)
-  }
+   passAge() {
+     this.passAgeCal = this.earthage - this.passbirth; 
+   }
 
-  yearsPassed() {
-    this.mercuryPassedbirth = Math.floor(this.passAgeCal / .24);
-    this.venusPassedbirth = Math.floor(this.passAgeCal / .62);
-    this.marsPassedbirth = Math.floor(this.passAgeCal / 1.88);
-    this.jupiterPassedbirth = Math.floor(this.passAgeCal / 11.86);
-  }
+   yearsPassed() {
+     this.mercuryPassedbirth = Math.floor(this.passAgeCal / .24);
+     this.venusPassedbirth = Math.floor(this.passAgeCal / .62);
+     this.marsPassedbirth = Math.floor(this.passAgeCal / 1.88);
+     this.jupiterPassedbirth = Math.floor(this.passAgeCal / 11.86);
+     
+   }
 
-  yearsToPass() {
-    this.earthAgeToPass = this.futurebirth - this.earthage
-  }
+   yearsToPass() {
+     this.earthAgeToPass = this.futurebirth - this.earthage
+   }
 
-  yearsToPassCal() {
+   yearsToPassCal() {
     this.mercuryYearsToPass = Math.floor(this.earthAgeToPass / .24);
     this.venusYearsToPass = Math.floor(this.earthAgeToPass / .62);
-    this.marsYearsToPass = Math.floor(this.earthAgeToPass/ 1.88);
+    this.marsYearsToPass = Math.floor(this.earthAgeToPass / 1.88);  
     this.jupiterYearsToPass = Math.floor(this.earthAgeToPass / 11.86);
-  }
+   }
 }
